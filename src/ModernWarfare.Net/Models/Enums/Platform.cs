7b3beafb54157887f8ef6d156c0843e8240f1a1f
Alexander Gipp <gipp81@gmail.com>
2020-04-09 21:40:44 +0200

    Added platform enums.

---
+++ src/ModernWarfare.Net/Models/Enums/Platform.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModernWarfare.Net.Models.Enums
+{
+    public enum Platform
+    {
+        BattleNet,
+        Activision,
+        PSN,
+        XBL
+    }
+}
